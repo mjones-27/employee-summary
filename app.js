d4e8f60db22af779a4bc2de897d62a1ffa32b117
unknown <mars.jones34@gmail.com>
2020-04-08 23:02:06 -0700

    all tests passing

--- app.js
+++ app.js
@@ -104,3 +104,4 @@ function newEmployee(){
 // for further information. Be sure to test out each class and verify it generates an 
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
+
